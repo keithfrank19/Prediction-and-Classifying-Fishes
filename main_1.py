d2810e5350f4ac1c59aed42c119029bb449ca2e9
Keith Frank <92335266+keithfrank19@users.noreply.github.com>
2022-03-31 19:16:28 -0400

    Update main_1.py

--- main_1.py
+++ main_1.py
@@ -2,7 +2,7 @@ from flask import Flask, request, url_for, redirect, render_template, jsonify
 import pandas as pd
 import pickle
 import numpy as np
-
+import os
 # Initalise the Flask app
 app = Flask(__name__)
 
